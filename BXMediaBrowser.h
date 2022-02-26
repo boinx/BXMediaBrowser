b9b3b39a286670cf2bc9eda1f71b380e0ea8cf2f
Peter Baumgartner <peter@boinx.com>
2022-02-26 09:39:33 +0100

    Added BXMediaBrowser.xcodeporj for framework

---
+++ BXMediaBrowser.h
@@ -0,0 +1,18 @@
+//
+//  BXMediaBrowser.h
+//  BXMediaBrowser
+//
+//  Created by Peter Baumgartner on 26.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BXMediaBrowser.
+FOUNDATION_EXPORT double BXMediaBrowserVersionNumber;
+
+//! Project version string for BXMediaBrowser.
+FOUNDATION_EXPORT const unsigned char BXMediaBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BXMediaBrowser/PublicHeader.h>
+
+
