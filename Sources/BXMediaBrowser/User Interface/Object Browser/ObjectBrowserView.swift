dafc04a6c4984de18af3c9c3a35dace613b20042
Peter Baumgartner <peter@boinx.com>
2022-04-01 10:45:10 +0200

    Added new SwiftUI ObjectBrowserView, which includes header, NSCollectionView, and footer

---
+++ Sources/BXMediaBrowser/User Interface/Object Browser/ObjectBrowserView.swift
@@ -0,0 +1,80 @@
+//----------------------------------------------------------------------------------------------------------------------
+//
+//  Copyright ©2022 Peter Baumgartner. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+//----------------------------------------------------------------------------------------------------------------------
+
+
+import SwiftUI
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+/// ObjectBrowserView displays a header, ObjectCollectionView, and a footer.
+///
+/// Depending on the currenty selected Container, different subviews will be displayed.
+
+public struct ObjectBrowserView : View
+{
+	// Model
+	
+	var container:Container?
+	@ObservedObject var uiState:UIState
+	
+	// Environment
+	
+	@Environment(\.viewFactory) private var viewFactory
+	
+	// Init
+	
+	public init(for container:Container?, uiState:UIState)
+	{
+		self.container = container
+		self.uiState = uiState
+	}
+	
+	// View
+	
+	public var body: some View
+    {
+		let cellType = viewFactory.objectViewControllerType(for:container, uiState:uiState)
+		
+		return VStack(spacing:0)
+		{
+			viewFactory.objectsHeaderView(for:container, uiState:uiState)
+				
+			Color.primary.opacity(0.15).frame(height:1) // Divider line
+			
+			ObjectCollectionView(container:container, cellType:cellType, uiState:uiState)
+			
+			Color.primary.opacity(0.15).frame(height:1) // Divider line
+
+			viewFactory.objectsFooterView(for:container, uiState:uiState)
+		}
+		.frame(minWidth:240, maxWidth:.infinity)
+   }
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
